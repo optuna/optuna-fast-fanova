69bc9c4199db70a5f522b979d8cf37f4929453b5
c-bata <contact@c-bata.link>
2022-05-12 01:11:28 +0900

    Update benchmark

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 optuna-fast-fanova provides Cython-accelerated version of [FanovaImportanceEvaluator](https://optuna.readthedocs.io/en/stable/reference/generated/optuna.importance.FanovaImportanceEvaluator.html#optuna.importance.FanovaImportanceEvaluator).
 
+| n_trials | n_params | n_trees      | fANOVA (Optuna) | fast-fanova     |
+|----------|----------|--------------|-----------------|-----------------|
+| 1000     | 32       | 64 (default) | 86.476s         | 8.325s (-90.4%) |
+| 1000     | 8        | 64 (default) | 112.277s        | 7.397s (-93.4%) |
+| 1000     | 2        | 64 (default) | 64.795s         | 4.503s (-93.1%) |
+| 100      | 32       | 64 (default) | 2.406s          | 0.769s (-68.0%) |
+| 100      | 8        | 64 (default) | 2.091s          | 0.578s (-72.3%) |
+| 100      | 2        | 64 (default) | 1.695s          | 0.381s (-77.5%) |
+| 1000     | 32       | 32           | 44.318s         | 4.805s (-89.2%) |
+| 1000     | 8        | 32           | 56.309s         | 3.718s (-93.4%) |
+| 1000     | 2        | 32           | 32.406s         | 2.654s (-91.8%) |
+| 100      | 32       | 32           | 1.191s          | 0.676s (-43.2%) |
+| 100      | 8        | 32           | 1.114s          | 0.292s (-73.8%) |
+| 100      | 2        | 32           | 0.815s          | 0.237s (-70.9%) |
+
+[The benchmark script](./benchmark.py) was run on my laptop so the times should not be taken precisely.
+
 ### Installation
 
 Supported Python versions are 3.7 or later.
@@ -39,16 +56,6 @@ You can use optuna-fast-fanova in only two steps.
 
 1. Add an import statement: `from optuna_fast_fanova import FanovaImportanceEvaluator`.
 2. Pass a `FanovaImportanceEvaluator()` object to an `evaluator` argument of `get_param_importances()` function.
-
-| trials / params | fANOVA (Optuna) | fast-fanova | 
-|-----------------|-----------------|-------------|
-| 1000 / 4        | 60.592s         | 4.690s      |
-| 1000 / 4        | 30.515s         | 2.393s      |
-| 1000 / 4        | 15.297s         | 1.225s      |
-
-[The benchmark script](./benchmark.py) was run on my laptop (M1 Pro Macbook) so the times should not be taken precisely.
-This library seems to be about more than 10-times faster than Optuna's FanovaImportanceExecutor (with Optuna v3.0.0b0).
-
 
 ## How to cite fANOVA
 
--- benchmark.py
+++ benchmark.py
@@ -1,43 +1,70 @@
-import os
+import itertools
+import math
 import time
 
 import optuna
 from optuna.importance import FanovaImportanceEvaluator
 
-from fast_fanova import FanovaImportanceEvaluator as FastFanovaImportanceEvaluator
+from optuna_fast_fanova import FanovaImportanceEvaluator as FastFanovaImportanceEvaluator
+
+optuna.logging.set_verbosity(optuna.logging.ERROR)
 
 
-def objective(trial):
-    x = trial.suggest_float("x", -100, 100)
-    y = trial.suggest_float("y", -1, 1)
-    z = trial.suggest_int("z", -10, 10)
-    foo = trial.suggest_categorical("foo", ["foo", "bar"])
-    return x**2 + y + z + len(foo)
+def run_optimize(storage, trials, params):
+    study_name = f"study-trial{trials}-params{params}"
+    study = optuna.create_study(storage=storage, study_name=study_name)
 
+    def objective(trial: optuna.Trial):
+        val = 0
+        for i in range(params):
+            xi = trial.suggest_float(str(i), -4, 4)
+            val += (xi - 2) ** 2
+        return val
 
-def create_study(filepath):
-    db_url = f"sqlite:///{filepath}"
-    if os.path.exists(filepath):
-        return optuna.load_study(storage=db_url, study_name="foo")
-
-    study = optuna.create_study(storage=db_url, study_name="foo")
-    study.optimize(objective, n_trials=1000)
+    study.optimize(objective, n_trials=trials)
     return study
 
 
-if __name__ == "__main__":
-    study = create_study("profile-fanova.db")
-    print("n_trials", study._storage.get_n_trials(study._study_id))
+def print_markdown_table(results):
+    print("| n_trials | n_params | n_trees | fANOVA (Optuna) | fast-fanova |")
+    print("| -------- | -------- | ------- | --------------- | ----------- |")
 
-    for n_trees in [1, 4, 8, 16, 32, 64]:
-        for (name, evaluator_cls) in [
-            # ("MeanDecreaseImpurity", MeanDecreaseImpurityImportanceEvaluator),
-            ("Fanova (Optuna)     ", FanovaImportanceEvaluator),
-            ("Fanova (FastFanova) ", FastFanovaImportanceEvaluator),
-        ]:
+    for n_trials, n_params, n_trees, s1, s2 in results:
+        print(f"| {n_trials} | {n_params} | {n_trees} | {s1:.3f}s | {s2:.3f}s (-{s1-s2/s1*100:.1f}%) |")
+
+
+def is_importance_close(a, b):
+    assert len(a) == len(b)
+    for k1, k2 in zip(a, b):
+        assert k1 == k2
+        assert math.isclose(a[k1], b[k2])
+
+
+def main():
+    storage = "sqlite:///benchmark-fanova.db"
+    results = []
+    for n_trials, n_params in itertools.product([1000, 100], [32, 8, 2]):
+        study = run_optimize(storage, n_trials, n_params)
+        for n_trees in [32, 64]:
             start = time.time()
-            importances = optuna.importance.get_param_importances(
-                study, evaluator=evaluator_cls(n_trees=n_trees, seed=0)
+            importances_before = optuna.importance.get_param_importances(
+                study, evaluator=FanovaImportanceEvaluator(n_trees=n_trees, seed=0)
             )
-            elapsed = time.time() - start
-            print(f"{name}\tn_trees={n_trees}\t{elapsed:.3f}s\t{dict(importances)}")
+            elapsed_before = time.time() - start
+
+            start = time.time()
+            importances_after = optuna.importance.get_param_importances(
+                study, evaluator=FastFanovaImportanceEvaluator(n_trees=n_trees, seed=0)
+            )
+            elapsed_after = time.time() - start
+
+            print(f"Before: n_trees={n_trees} elapsed={elapsed_before:.3f}\t{dict(importances_before)}")
+            print(f"After:  n_trees={n_trees} elapsed={elapsed_after:.3f}\t{importances_after}")
+            is_importance_close(importances_before, importances_after)
+
+            results.append((n_trials, n_params, n_trees, elapsed_before, elapsed_after))
+    print_markdown_table(results)
+
+
+if __name__ == '__main__':
+    main()
