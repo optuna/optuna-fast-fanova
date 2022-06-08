debce06892938b9d9a6193ffc0b76c6240b5573f
c-bata <contact@c-bata.link>
2022-06-08 16:57:38 +0900

    Update build_wheel.sh

--- tools/build_wheel.sh
+++ tools/build_wheel.sh
@@ -9,3 +9,5 @@ cd /io/
 /opt/python/cp39-cp39/bin/python3 -m pip install Cython==3.0.0a10 numpy scikit-learn && /opt/python/cp39-cp39/bin/python3 setup.py bdist_wheel
 /opt/python/cp38-cp38/bin/python3 -m pip install Cython==3.0.0a10 numpy scikit-learn && /opt/python/cp38-cp38/bin/python3 setup.py bdist_wheel
 /opt/python/cp37-cp37m/bin/python3 -m pip install Cython==3.0.0a10 numpy scikit-learn && /opt/python/cp37-cp37m/bin/python3 setup.py bdist_wheel
+
+for f in dist/*.whl; do auditwheel repair --plat manylinux2014_x86_64 $f; done
