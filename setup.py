e87abe1cc42d14b296da9cca04355bd776ddccf6
c-bata <contact@c-bata.link>
2022-09-10 16:40:45 +0900

    Fix lint errors

--- setup.py
+++ setup.py
@@ -10,14 +10,16 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         packages=find_packages(exclude=("tests", "tests.*")),
-        ext_modules=cythonize([
-            Extension(
-                "optuna_fast_fanova._fanova",
-                sources=[os.path.join("optuna_fast_fanova", "_fanova.pyx")],
-                include_dirs=[numpy.get_include()],
-                language="c",
-            )
-        ]),
+        ext_modules=cythonize(
+            [
+                Extension(
+                    "optuna_fast_fanova._fanova",
+                    sources=[os.path.join("optuna_fast_fanova", "_fanova.pyx")],
+                    include_dirs=[numpy.get_include()],
+                    language="c",
+                )
+            ]
+        ),
         include_package_data=False,
         package_data={"optuna_fast_fanova": ["*.pyx"]},
     )
