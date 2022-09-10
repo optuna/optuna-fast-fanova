36480e51532562857883facc08cbb9cee789176e
c-bata <contact@c-bata.link>
2022-09-10 16:26:30 +0900

    Remove build_ext to import numpy and Cython lazily

--- setup.py
+++ setup.py
@@ -1,36 +1,23 @@
 import os
 
+from Cython.Build import cythonize
+import numpy
 from setuptools import Extension
 from setuptools import find_packages
 from setuptools import setup
-from setuptools.command.build_ext import build_ext
-
-
-class LazyImportBuildExt(build_ext):
-    def finalize_options(self) -> None:
-        from Cython.Build import cythonize
-
-        self.extensions = cythonize(self.extensions)
-        super().finalize_options()
-
-    def run(self) -> None:
-        import numpy
-
-        self.include_dirs.append(numpy.get_include())
-        super().run()
 
 
 if __name__ == "__main__":
     setup(
         packages=find_packages(exclude=("tests", "tests.*")),
-        ext_modules=[
+        ext_modules=cythonize([
             Extension(
                 "optuna_fast_fanova._fanova",
                 sources=[os.path.join("optuna_fast_fanova", "_fanova.pyx")],
+                include_dirs=[numpy.get_include()],
                 language="c",
             )
-        ],
-        cmdclass={"build_ext": LazyImportBuildExt},
+        ]),
         include_package_data=False,
         package_data={"optuna_fast_fanova": ["*.pyx"]},
     )
